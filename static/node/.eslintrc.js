077fb87df4f50e53063b425f6cf5cb2bf3719cb3
tlemon <trevor.lemon@nike.com>
2021-04-04 11:24:55 -0700

    changed token format to support tokens in shell scripts

--- static/node/.eslintrc.js
+++ static/node/.eslintrc.js
@@ -6,20 +6,20 @@ module.exports = {
   },
   extends: 'eslint:recommended',
   overrides: [
-    //{TOKEN:^LINT__SVELTE}
+    //TOKEN:^LINT__SVELTE
     {
       files: ['*.svelte'],
       processor: 'svelte3/svelte3',
     }
-    //{TOKEN:$LINT__SVELTE}
+    //TOKEN:^LINT__SVELTE
   ],
   parserOptions: {
     ecmaVersion: 2019,
     sourceType: 'module',
   },
   plugins: [
-    //{TOKEN:^LINT__SVELTE}
+    //TOKEN:^LINT__SVELTE
     'svelte3',
-    //{TOKEN:$LINT__SVELTE}
+    //TOKEN:^LINT__SVELTE
   ],
 };
--- static/node/watcher.js
+++ static/node/watcher.js
@@ -1,45 +1,45 @@
 #!/usr/bin/env node
 
-//{TOKEN:^WATCHER__CLIENT}
+//TOKEN:^WATCHER__CLIENT
 const { create } = require('browser-sync');
-//{TOKEN:$WATCHER__CLIENT}
-//{TOKEN:^WATCHER__SERVER}
+//TOKEN:$WATCHER__CLIENT
+//TOKEN:^WATCHER__SERVER
 const nodemon = require('nodemon');
 const chokidar = require('chokidar');
-//{TOKEN:$WATCHER__SERVER}
-//{TOKEN:^WATCHER__LOGGER}
+//TOKEN:$WATCHER__SERVER
+//TOKEN:^WATCHER__LOGGER
 const logger = require('anylogger')('watcher');
-//{TOKEN:$WATCHER__LOGGER}
+//TOKEN:$WATCHER__LOGGER
 const { SERVER__PORT } = require('./src/constants');
 
-//{TOKEN:^WATCHER__SERVER}
+//TOKEN:^WATCHER__SERVER
 let httpModule;
 let protocol = 'http';
-//{TOKEN:$WATCHER__SERVER}
-//{TOKEN:^WATCHER__CLIENT}
+//TOKEN:$WATCHER__SERVER
+//TOKEN:^WATCHER__CLIENT
 const browserSync = create();
 let bSyncHTTPS;
-//{TOKEN:$WATCHER__CLIENT}
+//TOKEN:$WATCHER__CLIENT
 
 if (process.env.NODE_EXTRA_CA_CERTS) {
-  //{TOKEN:^WATCHER__SERVER}
+  //TOKEN:^WATCHER__SERVER
   httpModule = require('https');
   protocol = 'https';
-  //{TOKEN:$WATCHER__SERVER}
-  //{TOKEN:^WATCHER__CLIENT}
+  //TOKEN:$WATCHER__SERVER
+  //TOKEN:^WATCHER__CLIENT
   bSyncHTTPS = {
     cert: process.env.NODE_EXTRA_CA_CERTS,
     key: process.env.NODE_EXTRA_CA_CERTS.replace('.crt', '.key'),
   }
-  //{TOKEN:$WATCHER__CLIENT}
+  //TOKEN:$WATCHER__CLIENT
 }
-//{TOKEN:^WATCHER__SERVER}
+//TOKEN:^WATCHER__SERVER
 else {
   httpModule = require('http');
 }
-//{TOKEN:$WATCHER__SERVER}
+//TOKEN:$WATCHER__SERVER
 
-//{TOKEN:^WATCHER__SERVER}
+//TOKEN:^WATCHER__SERVER
 const checkServer = () => new Promise((rootResolve, rootReject) => {
   let count = 0;
   const check = () => new Promise((resolve, reject) => {
@@ -55,9 +55,9 @@ const checkServer = () => new Promise((rootResolve, rootReject) => {
         opts.rejectUnauthorized = false;
       }
       
-      //{TOKEN:^WATCHER__LOGGER}
+      //TOKEN:^WATCHER__LOGGER
       logger.info(`Pinging ${serverAddress}`);
-      //{TOKEN:$WATCHER__LOGGER}
+      //TOKEN:$WATCHER__LOGGER
       httpModule
         .get(serverAddress, opts, (res) => resolve(res))
         .on('error', (err) => reject(err));
@@ -69,9 +69,9 @@ const checkServer = () => new Promise((rootResolve, rootReject) => {
       count++;
     }
     else {
-      //{TOKEN:^WATCHER__LOGGER}
+      //TOKEN:^WATCHER__LOGGER
       logger.info(err);
-      //{TOKEN:$WATCHER__LOGGER}
+      //TOKEN:$WATCHER__LOGGER
       rootReject();
     }
   };
@@ -84,7 +84,7 @@ const checkServer = () => new Promise((rootResolve, rootReject) => {
 
   ping();
 });
-//{TOKEN:$WATCHER__SERVER}
+//TOKEN:$WATCHER__SERVER
 
 const fileCheck = (file, timeout = 5) => new Promise((resolveCheck, rejectCheck) => {
   const { existsSync } = require('fs');
@@ -95,20 +95,20 @@ const fileCheck = (file, timeout = 5) => new Promise((resolveCheck, rejectCheck)
 
   if (exists()) resolveCheck();
   else {
-    //{TOKEN:^WATCHER__LOGGER}
+    //TOKEN:^WATCHER__LOGGER
     logger.info(`Waiting for "${filePath}"\n to exist before starting.\n`);
-    //{TOKEN:$WATCHER__LOGGER}
+    //TOKEN:$WATCHER__LOGGER
     const int = setInterval(() => {
       elapsedTime++;
       
-      //{TOKEN:^WATCHER__LOGGER}
+      //TOKEN:^WATCHER__LOGGER
       logger.info('Looking for file');
-      //{TOKEN:$WATCHER__LOGGER}
+      //TOKEN:$WATCHER__LOGGER
       
       if (exists()) {
-        //{TOKEN:^WATCHER__LOGGER}
+        //TOKEN:^WATCHER__LOGGER
         logger.info('\nFile found, starting...\n');
-        //{TOKEN:$WATCHER__LOGGER}
+        //TOKEN:$WATCHER__LOGGER
         clearInterval(int);
         resolveCheck();
       }
@@ -126,24 +126,24 @@ const waitForFileBeforeStart = args[1];
 const fileGate = (waitForFileBeforeStart)
   ? fileCheck(waitForFileBeforeStart)
   : Promise.resolve();
-//{TOKEN:^WATCHER__SERVER}
+//TOKEN:^WATCHER__SERVER
 const watchedServerFiles = [
   './src/server/**/*.js',
   './src/constants.js',
 ];
-//{TOKEN:$WATCHER__SERVER}
+//TOKEN:$WATCHER__SERVER
 
 fileGate
   .then(() => {
-    //{TOKEN:^WATCHER__SERVER}
+    //TOKEN:^WATCHER__SERVER
     const serverFilesWatcher = chokidar.watch(watchedServerFiles, {
       ignoreInitial: true,
     });
     serverFilesWatcher
       .on('ready', () => {
-        //{TOKEN:^WATCHER__LOGGER}
+        //TOKEN:^WATCHER__LOGGER
         logger.info('Watching for Server changes');
-        //{TOKEN:$WATCHER__LOGGER}
+        //TOKEN:$WATCHER__LOGGER
       })
       .on('all', (ev, p) => { // events are: add addDir change unlink unlinkDir
         if (!serverFilesWatcher.events) serverFilesWatcher.events = [];
@@ -151,9 +151,9 @@ fileGate
 
         if (!serverFilesWatcher.debounce) {
           serverFilesWatcher.debounce = setTimeout(() => {
-            //{TOKEN:^WATCHER__LOGGER}
+            //TOKEN:^WATCHER__LOGGER
             logger.info(`Server updates:\n  - ${serverFilesWatcher.events.map(([_ev, _p]) => `${_ev}: ${_p}`).join('\n  - ')}`);
-            //{TOKEN:$WATCHER__LOGGER}
+            //TOKEN:$WATCHER__LOGGER
             delete serverFilesWatcher.debounce;
             delete serverFilesWatcher.events;
             
@@ -174,28 +174,28 @@ fileGate
       watch: watchedServerFiles,
     })
       .on('restart', () => {
-        //{TOKEN:^WATCHER__LOGGER}
+        //TOKEN:^WATCHER__LOGGER
         logger.info('Server restarting because file(s) changed');
-        //{TOKEN:$WATCHER__LOGGER}
+        //TOKEN:$WATCHER__LOGGER
     
         checkServer()
           .then(() => {
-            //{TOKEN:^WATCHER__LOGGER}
+            //TOKEN:^WATCHER__LOGGER
             logger.info('Server has fully started');
-            //{TOKEN:$WATCHER__LOGGER}
-            //{TOKEN:^WATCHER__CLIENT}
+            //TOKEN:$WATCHER__LOGGER
+            //TOKEN:^WATCHER__CLIENT
             browserSync.reload();
-            //{TOKEN:$WATCHER__CLIENT}
+            //TOKEN:$WATCHER__CLIENT
           })
           .catch(() => {
-            //{TOKEN:^WATCHER__LOGGER}
+            //TOKEN:^WATCHER__LOGGER
             logger.info("Couldn't detect the Server, a manual reload may be required");
-            //{TOKEN:$WATCHER__LOGGER}
+            //TOKEN:$WATCHER__LOGGER
           });
       });
-    //{TOKEN:$WATCHER__SERVER}
+    //TOKEN:$WATCHER__SERVER
     
-    //{TOKEN:^WATCHER__CLIENT}
+    //TOKEN:^WATCHER__CLIENT
     // https://www.browsersync.io/docs/options
     browserSync.init({
       files: [
@@ -222,19 +222,19 @@ fileGate
         port: SERVER__PORT + 2,
       },
     });
-    //{TOKEN:$WATCHER__CLIENT}
+    //TOKEN:$WATCHER__CLIENT
     
     function killWatcher(evType) {
-      //{TOKEN:^WATCHER__LOGGER}
+      //TOKEN:^WATCHER__LOGGER
       logger.info(`Killing watcher (${evType})`);
-      //{TOKEN:$WATCHER__LOGGER}
-      //{TOKEN:^WATCHER__CLIENT}
+      //TOKEN:$WATCHER__LOGGER
+      //TOKEN:^WATCHER__CLIENT
       browserSync.exit();
-      //{TOKEN:$WATCHER__CLIENT}
-      //{TOKEN:^WATCHER__SERVER}
+      //TOKEN:$WATCHER__CLIENT
+      //TOKEN:^WATCHER__SERVER
       serverFilesWatcher.close();
       nodemon.emit('quit');
-      //{TOKEN:$WATCHER__SERVER}
+      //TOKEN:$WATCHER__SERVER
       process.exit(0);
     }
     
@@ -243,9 +243,9 @@ fileGate
     process.on('SIGUSR2', killWatcher.bind(null, 'SIGUSR2'));
   })
   .catch(err => {
-    //{TOKEN:^WATCHER__LOGGER}
+    //TOKEN:^WATCHER__LOGGER
     logger.error(err);
-    //{TOKEN:$WATCHER__LOGGER}
+    //TOKEN:$WATCHER__LOGGER
     process.exit(1);
   });
   
