3340f9208a4adcc9ace21e94760e3c4174ef9e42
Nox <the0neWhoKnocks@users.noreply.github.com>
2021-04-08 13:35:18 -0700

    separated out shared and Server constants
    Ran into an issue where the WP bundle was erroring because a Server only var was referrencing `process.env` and not mapped in `DefinePlugin`.

--- static/node/constants.js
+++ static/node/constants.js
@@ -1,14 +1,21 @@
-module.exports = {
+// Shared by both Client and Server
+const constants = {
   APP__TITLE: '//TOKEN:#CONST__APP_TITLE',
-  DISCONNECT_TIMEOUT: 5000,
   //TOKEN:^CONST__SVELTE_MNT
   DOM__SVELTE_MOUNT_POINT: 'view',
   //TOKEN:$CONST__SVELTE_MNT
   //TOKEN:^CONST__LOGGER_NAMESPACE
   LOGGER__NAMESPACE: '//TOKEN:#CONST__LOGGER_NAMESPACE',
   //TOKEN:$CONST__LOGGER_NAMESPACE
-  SERVER__PORT: +process.env.SERVER_PORT || 3000,
   //TOKEN:^CONST__WS_MESSAGES
   WS__CLOSE_CODE__USER_REMOVED: 4000, // Close event numbers https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
   //TOKEN:$CONST__WS_MESSAGES
 };
+
+// Server only
+if (!process.env.FOR_CLIENT_BUNDLE) {
+  constants.DISCONNECT_TIMEOUT = 5000;
+  constants.SERVER__PORT = +process.env.SERVER_PORT || 3000;
+}
+
+module.exports = constants;
