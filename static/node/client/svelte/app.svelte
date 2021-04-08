319ddcaa1e9a9a2e08e56513550c996c92e63250
Nox <the0neWhoKnocks@users.noreply.github.com>
2021-04-08 15:46:11 -0700

    fleshed out Web Sockets

--- static/node/client/svelte/app.svelte
+++ static/node/client/svelte/app.svelte
@@ -1,11 +1,16 @@
 <script>
   import logger from '../utils/logger';
+  //TOKEN:^APP__WEB_SOCKET
   import {
     WS__CLOSE_CODE__USER_REMOVED,
+    WS__EXAMPLE_MSG,
   } from '../constants';
   import { connectToSocket } from './socket';
+  //TOKEN:$APP__WEB_SOCKET
   
   const log = logger('app');
+  //TOKEN:^APP__WEB_SOCKET
+  let wsData = [];
   
   async function init() {
     let socketAPI;
@@ -15,13 +20,59 @@
       socketAPI.on(WS__CLOSE_CODE__USER_REMOVED, () => {
         log('User disconnected');
       });
+      
+      socketAPI.on(WS__EXAMPLE_MSG, ({ msg }) => {
+        wsData = [...wsData, msg];
+      });
+      let count = 0;
+      const int = setInterval(() => {
+        if (count === 5) clearInterval(int);
+        else socketAPI.emit(WS__EXAMPLE_MSG, { d: Date.now() });
+        count++;
+      }, 1000);
+      
+      wsData = [...wsData, 'Connected to Server socket'];
     }
     catch(err) { log(err); }
   }
   
   init();
+  //TOKEN:$APP__WEB_SOCKET
 </script>
 
-<div>
-  Hello World
+<div class="app">
+  <div class="frame">
+    Hello World
+    <!--TOKEN:^APP__WEB_SOCKET -->
+    <pre class="ws-data">{wsData.join('\n')}</pre>
+    <!--TOKEN:$APP__WEB_SOCKET -->
+  </div>
 </div>
+
+<style>
+  .app {
+    width: 100%;
+    height: 100%;
+    background: #333;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .frame {
+    width: 30vw;
+    padding: 1em;
+    border-radius: 0.25em;
+    background: linear-gradient(#ccc, #aaa);
+  }
+  /*TOKEN:^APP__WEB_SOCKET */
+  .ws-data {
+    height: 10em;
+    color: #00e100;
+    padding: 0.25em 0.5em;
+    margin-bottom: 0;
+    background: #222;
+    display: block;
+  }
+  /*TOKEN:$APP__WEB_SOCKET */
+</style>
--- utils/replaceTokens.js
+++ utils/replaceTokens.js
@@ -1,13 +1,14 @@
 module.exports = function replaceTokens(src, tokens = []) {
-  const comment = '(?:\\/\\/|#)';
+  const commentStart = '(?:\\/\\/|#|<!--|\\/\\*)';
+  const commentEnd = '(?:\\s-->|\\s\\*\\/)?';
   let _src = src;
   
   tokens.forEach(({ remove, replacement, token }) => {
     if (replacement) {
-      _src = _src.replace(new RegExp(`${comment}TOKEN:#${token}`), replacement);
+      _src = _src.replace(new RegExp(`${commentStart}TOKEN:#${token}`), replacement);
     }
     else {
-      const regToken = `(?:^\\s+)?${comment}TOKEN:(?:\\^|\\$)${token}\\n`;
+      const regToken = `(?:^\\s+)?${commentStart}TOKEN:(?:\\^|\\$)${token}${commentEnd}\\n`;
       _src = _src.replace(
         new RegExp(`${regToken}(?<inner>[\\s\\S]*?)(?=${regToken})${regToken}`, 'gm'),
         remove ? '' : '$1'
