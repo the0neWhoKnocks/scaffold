c1021626d90d5d90972aad0e6e13cfe64c7f3746
Nox <the0neWhoKnocks@users.noreply.github.com>
2021-07-31 12:58:17 -0700

    fix: logger
    There were a couple issues (both possibly related to a Chrome update):
    1. Color function definitions were being output instead of the log type and namespace. Fixed it by using a custom `config.log_format`.
    2. Logs weren't always showing up unless I set the log level via a query param. Used to be able to just set a value in `localStorage`. Fixed it by setting `config.log`.
    
    Also noticed I was using a non-existent constant for the default logger.

--- static/node/utils/logger.js
+++ static/node/utils/logger.js
@@ -1,10 +1,9 @@
-require('ulog');
 const { NAMESPACE__LOGGER } = require('../constants');
 
 //TOKEN:^LOGGER__DEFAULT
 function l(namespace) {
   const lg = (...args) => {
-    console.log(`[${LOGGER__NAMESPACE}:${namespace}]`, ...args);
+    console.log(`[${NAMESPACE__LOGGER}:${namespace}]`, ...args);
   };
   
   function _l(...args) {
@@ -21,6 +20,30 @@ function l(namespace) {
 const logger = l;
 //TOKEN:$LOGGER__DEFAULT
 //TOKEN:^LOGGER__CUSTOM
+const ulog = require('ulog');
+// NOTE: Overriding for both Server and Client because of the noted issues.
+// - If the Client issue ever gets fixed, the Server only logic for
+//   `process.env.FOR_CLIENT_BUNDLE` will have to be reimplemented.
+// Issues:
+// - Client: https://github.com/Download/ulog/issues/69
+// - Format padding: https://github.com/Download/ulog/issues/68
+ulog.config.log_format = 'lvl noPadName message';
+if (process.env.FOR_CLIENT_BUNDLE) {
+  ulog.config.log = 'debug';
+  window.localStorage.log = ulog.config.log;
+  window.localStorage.log_format = ulog.config.log_format;
+}
+ulog.use({
+  use: [ require('ulog/mods/formats') ],
+  formats: {
+    noPadName: () => {
+      const fmt = (rec) => rec.name;
+      fmt.color = 'logger';
+      return fmt;
+    },
+  },
+});
+
 const aL = require('anylogger');
 const rootLogger = aL(NAMESPACE__LOGGER);
 
