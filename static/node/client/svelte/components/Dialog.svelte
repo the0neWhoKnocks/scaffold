42653878f0a0fc8d3731697ff0288672b86482c7
Trevor Lemon <duhlemman.dev@gmail.com>
2023-11-16 13:32:54 -0800

    ops: Update Dialog

--- static/node/client/svelte/components/Dialog.svelte
+++ static/node/client/svelte/components/Dialog.svelte
@@ -1,5 +1,9 @@
+<script context="module">
+  let dialogNum = 0;
+</script>
 <script>
   import { cubicOut } from 'svelte/easing'; // visualizations https://svelte.dev/repl/6904f0306d6f4985b55f5f9673f762ef?version=3.4.1
+  import Portal from 'svelte-portal';
   
   export let animDuration = 300;
   export let bodyColor = '#eee';
@@ -7,9 +11,13 @@
   export let modal = false;
   export let onCloseClick = undefined;
   export let onCloseEnd = undefined;
+  export let onOpenEnd = undefined;
   export let title = '';
   export let titleBGColor = '#333';
   export let titleTextColor = '#eee';
+  
+  dialogNum += 1;
+  let dNum = dialogNum;
   
   const cssVars = `
     --dialog-anim-duration: ${animDuration}ms;
@@ -19,7 +27,7 @@
     --dialog-title-text-color: ${titleTextColor};
   `;
   
-  const toggleDialog = (node, { dir, start }) => {
+  const toggleDialog = (_, { dir, start }) => {
     const diff = 20;
     return {
       duration: animDuration,
@@ -45,6 +53,11 @@
   
   function handleCloseEnd() {
     if (onCloseEnd) onCloseEnd();
+    dialogNum -= 1;
+  }
+  
+  function handleOpenEnd() {
+    if (onOpenEnd) onOpenEnd();
   }
   
   function handleCloseClick() {
@@ -53,57 +66,60 @@
   
   function handleKeyDown({ key }) {
     switch (key) {
-      case 'Escape':
-        handleCloseClick();
+      case 'Escape': {
+        if (dNum === dialogNum) handleCloseClick();
         break;
+      }
     }
   }
 </script>
 
 <svelte:window on:keydown={handleKeyDown}/>
 
-<div 
-  class="dialog-wrapper"
-  style={cssVars}
->
-  <div
-    class="dialog-mask"
-    aria-hidden="true"
-    on:click={handleCloseClick}
-    in:toggleMask
-    out:toggleMask
-  ></div>
-  <dialog
-    class="dialog"
-    class:is--modal={modal}
-    open
-    in:toggleDialog="{{ dir: 'in', start: 70 }}"
-    out:toggleDialog="{{ start: 50 }}"
-    on:outroend={handleCloseEnd}
+<Portal target="#overlays">
+  <div 
+    class="dialog-wrapper"
+    style={cssVars}
   >
-    {#if !modal || modal && title}
-      <nav class="dialog__nav">
-        <div class="dialog__title">
-          <slot name="dialogTitle">{title}</slot>
-        </div>
-        {#if !modal}
-          <button
-            type="button"
-            class="dialog__close-btn"
-            on:click={handleCloseClick}
-          >&#10005;</button>
-        {/if}
-      </nav>
-    {/if}
-    <div class="dialog__body">
-      <slot name="dialogBody"></slot>
-    </div>
-  </dialog>
-</div>
+    <div
+      class="dialog-mask"
+      aria-hidden="true"
+      on:click={handleCloseClick}
+      in:toggleMask
+      out:toggleMask
+    ></div>
+    <dialog
+      class="dialog"
+      class:is--modal={modal}
+      open
+      in:toggleDialog="{{ dir: 'in', start: 70 }}"
+      out:toggleDialog="{{ start: 50 }}"
+      on:introend={handleOpenEnd}
+      on:outroend={handleCloseEnd}
+    >
+      {#if !modal || modal && (title || $$slots.dialogTitle)}
+        <nav class="dialog__nav">
+          <div class="dialog__title">
+            <slot name="dialogTitle">{title}</slot>
+          </div>
+          {#if !modal}
+            <button
+              type="button"
+              class="dialog__close-btn"
+              on:click={handleCloseClick}
+            >&#10005;</button>
+          {/if}
+        </nav>
+      {/if}
+      <div class="dialog__body">
+        <slot name="dialogBody"></slot>
+      </div>
+    </dialog>
+  </div>
+</Portal>
 
 <style>
   .dialog-wrapper {
-    font: 16px Helvetica, Arial, sans-serif;
     position: fixed;
     top: 0;
     left: 0;
@@ -123,33 +139,13 @@
   .dialog-wrapper :global(textarea) {
 		fill: orange;
 	}
-  .dialog-wrapper :global(button:not(disabled)) {
+  .dialog-wrapper :global(button:not(:disabled)) {
     cursor: pointer;
-  }
-  .dialog__body :global(button) {
-    color: #fff;
-    width: 100%;
-    padding: 0.75em 1em;
-    border: none;
-    border-radius: 0.25em;
-    background: #000;
-    position: relative;
-  }
-  .dialog__body :global(button:focus) {
-    outline: none;
-  }
-  .dialog__body :global(button:focus::after) {
-    content: '';
-    position: absolute;
-    border: solid 2px currentColor;
-    border-radius: 0.25em;
-    top: 2px;
-    left: 2px;
-    bottom: 2px;
-    right: 2px;
   }
   
   .dialog {
+    max-width: calc(100vw - 4em); /* edge spacing in case content is large */
+    max-height: calc(100vh - 4em);
     overflow: hidden;
     padding: 0;
     border: solid 4px var(--dialog-border-color);
@@ -157,6 +153,8 @@
     margin: 0;
     background: var(--dialog-border-color);
     box-shadow: 0 0.75em 2em 0.25em rgba(0, 0, 0, 0.75);
+    display: flex;
+    flex-direction: column;
     position: absolute;
     top: 50%;
     left: 50%;
@@ -169,6 +167,7 @@
     border-bottom: solid 1px;
     background-color: var(--dialog-title-bg-color);
     display: flex;
+    align-items: center;
   }
   
   .dialog__title {
@@ -180,12 +179,13 @@
   }
   
   .dialog__body {
+    overflow: hidden;
     background: var(--dialog-body-color);
+    display: flex;
   }
   
   .dialog__close-btn {
     color: var(--dialog-title-text-color);
-    padding: 0 1em;
     border: none;
     background: var(--dialog-title-bg-color);
   }
