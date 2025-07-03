1617f3d9a7ac8c74dda3b1ee3f4c70d66a0063c1
Nox <the0neWhoKnocks@users.noreply.github.com>
2025-07-03 11:33:16 -0700

    fix: 'plugin:svelte' added to '.eslintrc.js' when no Client added.

--- static/node/.eslintrc.js
+++ static/node/.eslintrc.js
@@ -7,7 +7,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:node/recommended',
+    //TOKEN:^LINT__SVELTE
     'plugin:svelte/recommended',
+    //TOKEN:$LINT__SVELTE
   ],
   overrides: [],
   parserOptions: {
